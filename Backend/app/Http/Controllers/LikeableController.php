e3d9d9b77e6ef9af8a5126922b8e3f8f96886e3c
G1ovann16 <glandaburo92@gmail.com>
2020-06-06 01:55:20 +0200

    created backend with cmr

---
+++ Backend/app/Http/Controllers/LikeableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Likeable;
+use Illuminate\Http\Request;
+
+class LikeableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Likeable  $likeable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Likeable $likeable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Likeable  $likeable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Likeable $likeable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Likeable  $likeable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Likeable $likeable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Likeable  $likeable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Likeable $likeable)
+    {
+        //
+    }
+}
