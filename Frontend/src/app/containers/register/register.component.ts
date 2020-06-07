e0ad28f4004dd8e30feb16ea6a2bd08377fca46c
G1ovann16 <glandaburo92@gmail.com>
2020-06-07 14:27:43 +0200

    made register

--- Frontend/src/app/containers/register/register.component.ts
+++ Frontend/src/app/containers/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { Router } from '@angular/router';
+import {UserService} from '../../services/user.service';
+import { NgForm } from '@angular/forms';
+// import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+    private router: Router,
+    // private notification: NzNotificationService,
+  ) { }
 
   ngOnInit(): void {
   }
+  register(registerform: NgForm) {
 
+    {
+      console.log(registerform.value);
+      this.userService.setNewRegister(registerform.value)
+        .subscribe(() => {
+          setTimeout(() => this.router.navigate(['/login']), 2000);
+        },
+        (error: HttpErrorResponse) => {
+        });
+
+  }
+
+  }
 }
