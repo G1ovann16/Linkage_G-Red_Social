ad2797ccbb2c6edf370dd81cec858aa112f4db1a
G1ovann16 <glandaburo92@gmail.com>
2020-06-07 14:22:06 +0200

    made login

--- Frontend/src/app/containers/login/login.component.ts
+++ Frontend/src/app/containers/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
+import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userService: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  loginIn(userform: NgForm) {
+    // if (!userform.valid){
+    //     return this.notification.warning('Empty field', 'completa todo anda`');
+    //   }
+    {
+      console.log(userform.value);
+      this.userService.setNewLogin(userform.value)
+        .subscribe((res: HttpResponse<object>) => {
+          localStorage.setItem('authToken', res['user']['token']);
+          localStorage.setItem('User', res['user']['id']);
+          setTimeout(() => this.router.navigate(['/home']), 2000);
+        },
+          (error: HttpErrorResponse) => {
+            console.error(error);
+            console.log('Wrong Login', 'There was a problem trying to log in');
+          });
+    }
+  }
 }
